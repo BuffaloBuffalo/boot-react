d19d07c45ee42e77958c3789c678c8f61357a38c
Jeff Keller <jrkelle27@gmail.com>
2015-10-30 14:57:17 -0400

    * Removed unneccesary front end code for simple case. * Updated the basic index.js to hava a simple data list that can be added to.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,50 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import initStore from 'config/store';
-import RouterComponent from 'config/router';
 import axios from 'axios';
-import axiosConfig from 'config/axios';
-
-axiosConfig();
-
-var render = (session) => {
-  const initialState = {
-    authentication: {
-      token: session.token || null,
-      isAuthenticated: session.isAuthenticated || false,
-      username: session.username || null
+  
+  class DataPoint extends React.Component{
+    render(){
+      return <li>{this.props.value}</li>
     }
-  };
-  const store = initStore(initialState);
+  }
+
+  class DataList extends React.Component{
+    render(){
+      return <ul>
+              {this.props.data.map(function(dataPoint) {
+                return <DataPoint key={dataPoint} value={dataPoint}/>
+              })}
+            </ul>
+    }
+  }
+
+  class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {data: props.initialData};
+    }
+
+
+   render(){
+      var self = this;
+      return <div>
+                <DataList data={this.state.data}/>
+                <button onClick={self.handleClick.bind(self)}>Generate New Value</button>
+              </div>
+    }
+    handleClick(event){
+      console.log('addValue',event);
+      var app = this;
+      axios.get('/api/simple').then(function(newVal){
+        console.log('generated new value ',newVal);
+        app.setState({data:app.state.data.concat(newVal.data)});
+      });
+    }
+  }
+
+  var initialData = ['Alpha','Beta','Gamma']
 
   ReactDOM.render(
-    <RouterComponent store={store}/>,
+    <App initialData={initialData}/>,
     document.getElementById('root')
   );
-};
-
-axios.get('/api/session')
-  .then(res => render(res.data))
-  .catch(err => render());
-
--- frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import simple from './simple';
-import authentication from './authentication';
-
-export default combineReducers({
-  simple,
-  authentication
-});
